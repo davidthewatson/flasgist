94e6e68671248ce5a5f4b52674768303813a371b
David Watson <davidthewatson@gmail.com>
2012-01-04 23:34:33 -0500

    added redirect for evans ec article

--- views.py
+++ views.py
@@ -103,6 +103,7 @@ def software():
 @app.route('/2007/01/happy-new-year-mythtv.html')
 @app.route('/2007/07/you-have-entered-an-invalid-value.html')
 @app.route('/2008/11/acer-restore-failed-reason-0xd0000017.html')
+@app.route('/2007/02/on-merits-of-evans-ec-snare-drum-head.html')
 def redirect_to_new_page():
     page = str.replace(request.url[request.url.rfind('/')+1:], '-', '_')
     return redirect(url_for('page', page=page), 301)
